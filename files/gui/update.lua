00028b6515ce22ab91f0cac9594b76b4b54f293b
shoozzzh <shuggermail@gmail.com>
2024-10-25 17:13:04 +0800

    version 1.6.7

--- files/gui/update.lua
+++ files/gui/update.lua
@@ -41,7 +41,7 @@ if initialized == false then
 		return GameTextGetTranslatedOrNot( wrap_key( key ) )
 	end
 
-	local version = "Shugged v1.6.6"
+	local version = "Shugged v1.6.7"
 
 	function maxn( t )
 		local result = table.maxn( t )
