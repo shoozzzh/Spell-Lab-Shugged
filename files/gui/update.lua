9088bec6de28b5fa9c5820009d507b41cbd0cc13
Shug <shuggermail@gmail.com>
2024-11-16 00:44:18 +0800

    fix sound turning on/off gif mode

--- files/gui/update.lua
+++ files/gui/update.lua
@@ -890,6 +890,7 @@ if initialized == false then
 						local description = wrap_key( gif_mode and "gif_mode_disable" or "gif_mode_enable" )
 						local left_click, right_click = do_flag_toggle_image_button( "mods/spell_lab_shugged/files/gui/buttons/show_wand_edit_panel.png", "show_wand_edit_panel", "wand_edit_panel", nil, description )
 						if right_click then
+							sound_button_clicked()
 							mod_setting_set( "gif_mode", not gif_mode )
 						end
 					end
