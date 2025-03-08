4f0891ce9c55d9024e1d3d25e74bdfdd08796dba
Shug <shuggermail@gmail.com>
2025-03-08 16:03:09 +0800

    version 1.8.3

--- files/gui/update.lua
+++ files/gui/update.lua
@@ -21,7 +21,7 @@ gui = gui or GuiCreate()
 GuiStartFrame( gui )
 screen_width, screen_height = GuiGetScreenDimensions( gui )
 
-local version = "Shugged v1.8.2"
+local version = "Shugged v1.8.3"
 
 type_text = {
 	[ACTION_TYPE_MODIFIER]          = "$inventory_actiontype_modifier",
