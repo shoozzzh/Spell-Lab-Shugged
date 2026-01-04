e29ad5fc89be2b62bee006350f089e742f8175cb
Shug <shuggermail@gmail.com>
2026-01-05 02:28:13 +0800

    more rearrangement

---
+++ files/constants.lua
@@ -0,0 +1,17 @@
+
+Type_Adjustment = {
+	Add = 1,
+	Set = 2,
+}
+
+EditPanelTags = {
+	Init = "spell_lab_shugged.edit_panel_init",
+	Recording = "spell_lab_shugged.recording_history_state",
+	UncachedChanges = "spell_lab_shugged.uncached_changes",
+	History = "spell_lab_shugged.history",
+}
+
+VFiles = {
+	FinishingDumping = "mods/spell_lab_shugged/vfiles/is_dumping.txt",
+	WandId = "mods/spell_lab_shugged/vfiles/load_to_this_wand.txt",
+}
--- libs/helper.lua
+++ libs/helper.lua
@@ -324,23 +324,6 @@ end
 
 dofile_once_wrapped = memoize( dofile_wrapped )
 
-Type_Adjustment = {
-	Add = 1,
-	Set = 2,
-}
-
-EditPanelTags = {
-	Init = "spell_lab_shugged.edit_panel_init",
-	Recording = "spell_lab_shugged.recording_history_state",
-	UncachedChanges = "spell_lab_shugged.uncached_changes",
-	History = "spell_lab_shugged.history",
-}
-
-VFiles = {
-	FinishingDumping = "mods/spell_lab_shugged/vfiles/is_dumping.txt",
-	WandId = "mods/spell_lab_shugged/vfiles/load_to_this_wand.txt",
-}
-
 dofile_once( "mods/spell_lab_shugged/files/lib/stream.lua" )
 
 function stream_actions( wand_id )
